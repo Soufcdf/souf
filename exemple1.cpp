a20d9c4d1f79988fe55be676b4e11cf5f80ee5ac
Soufiane Ouriaghli <soufiane.ouriaghli@cdfnancy.fr>
2023-09-29 10:48:31 +0200

    version polis

--- exemple1.cpp
+++ exemple1.cpp
@@ -7,6 +7,7 @@ Fonction principale
 *****************************/
 int main()// Fonction principale
 {
+    cout<< "bonjour lekip"<<endl;
 cin.get() ; cin.ignore() ; // attente d'appui sur une touche
 return EXIT_SUCCESS; // fin du programme. Le code 0 est envoyé
 }
