60ec2d1b24ca254e0ac16babff1eeeb98459adf2
Soufiane Ouriaghli <soufiane.ouriaghli@cdfnancy.fr>
2023-09-29 10:38:53 +0200

    programme de depart

---
+++ exemple1.cpp
@@ -0,0 +1,12 @@
+/***************************
+*****************************/
+#include <iostream> // bibliothèque de gestion des E/S
+using namespace std; // utilisation de l'espace de nommage standard
+/*****************************
+Fonction principale
+*****************************/
+int main()// Fonction principale
+{
+cin.get() ; cin.ignore() ; // attente d'appui sur une touche
+return EXIT_SUCCESS; // fin du programme. Le code 0 est envoyé
+}
